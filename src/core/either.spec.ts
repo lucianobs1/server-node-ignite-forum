e086edd5510eed4eceb87b396ee11c3b0feb5bcf
lucianobs1 <lucianotetsu@gmail.com>
2023-07-29 13:43:46 -0300

    Functional Error Handling - Testando classes de erro

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+import { Either, left, right } from './either';
+
+function doSomething(shouldSuccess: boolean): Either<string, string> {
+  if (shouldSuccess) {
+    return right('success');
+  } else {
+    return left('error');
+  }
+}
+
+test('success result', () => {
+  const result = doSomething(true);
+
+  if (result.isRight()) {
+    console.log(result.value);
+  }
+
+  expect(result.isLeft()).toEqual(false);
+  expect(result.isRight()).toEqual(true);
+});
+
+test('error result', () => {
+  const result = doSomething(false);
+
+  expect(result.isLeft()).toEqual(true);
+  expect(result.isRight()).toEqual(false);
+});
